7951ff98cfdf6361ce7bb67a02c0d8a926e74abf
white-elephant-li <390023206@qq.com>
2021-05-10 08:19:42 +0800

    add banner but not good

--- vue-web/src/components/Home.vue
+++ vue-web/src/components/Home.vue
@@ -1,12 +1,32 @@
 <template>
     <div class="home">
-        <h1>Home</h1>
+        <div class="home-content">
+            <my-swiper :swiperImgs="swiperImgs" :height="swiperHeight"></my-swiper>
+        </div>
     </div>
 </template>
 
 <script>
+import MySwiper from '@c/swiper/MySwiper.vue'
 export default {
-
+    components:{
+        MySwiper,
+    },
+    data: function(){
+        return{
+            swiperImgs: [
+                require('@img/swiper-1.jpg'),
+                require('@img/swiper-2.jpg'),
+                require('@img/swiper-3.jpg'),
+                require('@img/swiper-4.jpg'),
+                require('@img/swiper-5.jpg'),
+                require('@img/swiper-6.jpg'),
+                require('@img/swiper-7.jpg'),
+                require('@img/swiper-8.jpg'),
+            ],
+            swiperHeight: '184px'
+        }
+    }
 }
 </script>
 
@@ -14,6 +34,9 @@ export default {
     .home{
         width: 100%;
         height: 100%;
-        font-size: 32px;
+        
+        &-content{
+            height: 100%;
+        }
     }
 </style>
--- vue-web/src/components/swiper/MySwiper.vue
+++ vue-web/src/components/swiper/MySwiper.vue
@@ -1,13 +1,70 @@
 <template>
-  
+  <div>
+      <swiper :options="swiperOptions">
+          <swiper-slide v-for="(item,index) in swiperImgs" :key="index">
+              <img class="swiper-slide-img"  :style="{height: height}" :src="item" alt="" srcset=""/>
+          </swiper-slide>
+          <template v-slot:pagination>
+              <div class="swiper-pagination">
+
+              </div>
+          </template>
+      </swiper>
+  </div>
 </template>
 
 <script>
+import 'swiper/dist/css/swiper.css'
+import {swiper,swiperSlide} from 'vue-awesome-swiper'
 export default {
-
+    props: {
+        height: {
+            type: String,
+            default: 'auto'
+        },
+        // 数据源
+        swiperImgs: {
+            type: Array,
+            required: true,
+            default: function(){
+                return []
+            }
+        }
+    },
+    components: {
+        swiper,
+        swiperSlide
+    },
+    data: function(){
+        return {
+            swiperOptions: {
+                autoplay: true,
+                autoHeight: true,
+                pagination: {
+                    el: '.swiper-pagination',
+                    type: 'bullets',
+                    bulletClass: 'custom-bullet-class'
+                }
+            }
+        }
+    }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '@css/style.scss';
+    .swiper-pagination{
+        bottom: px2rem(12);
 
+        .custom-bullet-class {
+            box-sizing: border-box;
+            border-radius: 100%;
+            height: px2rem(6);
+            width: px2rem(6);
+            border: px2rem(1) solid #fff;
+            margin: 0 px2rem(4);
+            display: inline-block;
+            opacity: 1;
+        }
+    }
 </style>
